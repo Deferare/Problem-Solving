1fb4a70253ada533d866e48c73d3fe3c4ce7f7f8
Ubinyou <73418225+Ubinyou@users.noreply.github.com>
2021-06-15 11:07:34 +0900

    Create 11399_ATM.java

---
+++ BaekJoon/11399_ATM.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n]; int sum = 0;
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for (int i = 0; i < n-1; i++) {
+			for (int j = 0; j < n-1; j++) {
+				if (arr[j] > arr[j+1]) {
+					int tmp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
+				}
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i+1; j++) {
+				sum += arr[j];
+			}			
+		}
+		System.out.println(sum);
+	}
+}
