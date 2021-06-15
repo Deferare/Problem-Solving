66565bbc7369224c750a4822682f99ba25df62b6
Ubinyou <73418225+Ubinyou@users.noreply.github.com>
2021-06-15 10:46:48 +0900

    Create 10430_나머지.kt

---
+++ BaekJoon/10430_나머지.kt
@@ -0,0 +1,11 @@
+import java.util.*
+fun main() {
+    val sc = Scanner(System.`in`)
+    val a = sc.nextInt()
+    val b = sc.nextInt()
+    val c = sc.nextInt()
+    println((a+b)%c)
+    println(((a%c) + (b%c))%c)
+    println((a*b)%c)
+    println(((a%c)*(b%c))%c)
+}
