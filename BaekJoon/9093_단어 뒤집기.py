e86950ac020af7c37bc4a52889842888f19a789a
Ubinyou <73418225+Ubinyou@users.noreply.github.com>
2021-06-15 10:02:47 +0900

    Create 9093_단어 뒤집기.py

---
+++ BaekJoon/9093_단어 뒤집기.py
@@ -0,0 +1,13 @@
+test_case = int(input())
+for _ in range(test_case):
+    str = input()
+    save_str = ""
+    sub_str = ""
+    for crnt in str:
+        sub_str += crnt
+        if crnt == " ":
+            save_str += sub_str[::-1]
+            sub_str = ""
+    sub_str += " "
+    save_str += sub_str[::-1]
+    print(save_str[1:])
