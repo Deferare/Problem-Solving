bdd602ff6963a0a8dd0392a548dcc30cd4fa0aaa
Ubinyou <73418225+Ubinyou@users.noreply.github.com>
2021-06-15 11:01:09 +0900

    Create 2743_단어 길이 재기.java

---
+++ BaekJoon/2743_단어 길이 재기.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		String n = sc.nextLine();
+		System.out.print(n.length());
+	}
+}
