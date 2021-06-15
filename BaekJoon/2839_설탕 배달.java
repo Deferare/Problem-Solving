03f46d3eff1fab5f300503101390ea843d5a73d0
Ubinyou <73418225+Ubinyou@users.noreply.github.com>
2021-06-15 11:08:14 +0900

    Create 2839_설탕 배달.java

---
+++ BaekJoon/2839_설탕 배달.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int x = 0;		
+		while(true) {
+			if (n%5 == 0) {
+				System.out.println(n/5 + x);
+				break;
+			}
+			else if (n <= 0) {
+				System.out.println(-1);
+				break;
+			}
+			n -= 3;
+			x++;
+		}				
+	}
+}
