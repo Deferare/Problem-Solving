d9f5014d2f5a7d722340a3969b14120fa03e093b
KANG SEUNG HOON <73418225+Ubinyou@users.noreply.github.com>
2021-07-31 15:44:56 +0900

    Update 4889_안정적인 문자열.py

--- BaekJoon/4889_안정적인 문자열.py
+++ BaekJoon/4889_안정적인 문자열.py
@@ -6,21 +6,19 @@ while True:
     check = 0
     cnt_check += 1
     braces_arr = list(map(str,stdin.readline().strip()))
-    sub_save = []
-    left_stack = []
+    sub_save = 0
+    left_save = 0
     for i in range(len(braces_arr)):
         if braces_arr[i] == "{":
-            left_stack.append(i)
+            left_save += 1
         elif braces_arr[i] == "-":
             check = 1
             break
-        elif len(left_stack) > 0:
-            left_stack.pop()
-        elif len(left_stack) == 0:
-            sub_save.append(i)
+        elif left_save > 0:
+            left_save -= 1
+        elif left_save == 0:
+            sub_save += 1
     if check == 1:
         break
-    left_len = len(left_stack)
-    sub_len = len(sub_save)
-    answer = ((left_len//2+left_len%2) + (sub_len//2+sub_len%2))
+    answer = ((left_save//2+left_save%2) + (sub_save//2+sub_save%2))
     print(f"{cnt_check}. {answer}")
