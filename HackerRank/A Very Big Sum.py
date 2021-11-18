25524b23dd159abf923f6f1dc167f1ca8bc069c5
Deforeturn <deforeturn@gmail.com>
2021-11-19 00:37:13 +0900

    HackerRank start 4th clear.

---
+++ HackerRank/A Very Big Sum.py
@@ -0,0 +1,5 @@
+def aVeryBigSum(ar):
+    result = 0
+    for i in range(ar_count):
+        result += ar[i]
+    return result
---
+++ HackerRank/Compare the Triplets.py
@@ -0,0 +1,8 @@
+def compareTriplets(a, b):
+    result = [0,0]
+    for i in range(len(a)):
+        if a[i] > b[i]:
+            result[0] += 1
+        elif a[i] < b[i]:
+            result[1] += 1
+    return result
