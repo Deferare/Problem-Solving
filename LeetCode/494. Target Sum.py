a977153d8bc5e4b580e91129e5a8d6ded46cb1c7
Deforeturn <73418225+Deforeturn@users.noreply.github.com>
2021-12-09 16:25:50 +0900

    Create 494. Target Sum.py

---
+++ LeetCode/494. Target Sum.py
@@ -0,0 +1,13 @@
+from collections import Counter
+class Solution:
+    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+        counts = Counter()
+        counts[-nums[0]] += 1
+        counts[nums[0]] += 1
+        for num in nums[1:]:
+            new = Counter()
+            for key in counts.keys():
+                new[key + num] += counts[key]
+                new[key - num] += counts[key]
+            counts = new
+        return counts[S]
