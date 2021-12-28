b3047ddf87803fb841464bcbb72f424229371d47
Deforeturn <73418225+Deforeturn@users.noreply.github.com>
2021-12-28 12:24:37 +0900

    Create 297. Serialize and Deserialize Binary Tree.py

---
+++ LeetCode/297. Serialize and Deserialize Binary Tree.py
@@ -0,0 +1,33 @@
+from collections import deque
+class Codec:
+    def serialize(self, root: TreeNode) -> str:
+        queue = deque([root])
+        result = ['#']
+        while queue:
+            node = queue.popleft()
+            if node:
+                queue.append(node.left)
+                queue.append(node.right)
+                result.append(str(node.val))
+            else:
+                result.append('#')
+        return ' '.join(result)
+
+    def deserialize(self, data: str) -> TreeNode:
+        if data == '# #':
+            return None
+        nodes = data.split()
+        root = TreeNode(int(nodes[1]))
+        queue = deque([root])
+        index = 2
+        while queue:
+            node = queue.popleft()
+            if nodes[index] != '#':
+                node.left = TreeNode(int(nodes[index]))
+                queue.append(node.left)
+            index += 1
+            if nodes[index] != '#':
+                node.right = TreeNode(int(nodes[index]))
+                queue.append(node.right)
+            index += 1
+        return root
